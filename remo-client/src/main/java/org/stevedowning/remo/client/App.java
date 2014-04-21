e5c602f6fe26fb3e942cc7f2dae972313c015bf0
Steve Downing <ssdowning@gmail.com>
2014-04-20 23:54:58 -0700

    Initial project skeleton

---
+++ remo-client/src/main/java/org/stevedowning/remo/client/App.java
@@ -0,0 +1,13 @@
+package org.stevedowning.remo.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
