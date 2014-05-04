83af8974045230a9d1bf8521e5f8f6c7a6adabb3
Steve Downing <ssdowning@gmail.com>
2014-05-04 13:32:16 -0700

    createRemoteServiceHook throws an exception

--- remo-client/src/main/java/org/stevedowning/remo/client/RemoClientFactory.java
+++ remo-client/src/main/java/org/stevedowning/remo/client/RemoClientFactory.java
@@ -8,5 +8,5 @@ public interface RemoClientFactory {
     public <T> T getRemoteService(Class<T> serviceType, String hostname, int port)
             throws IOException;
     public <T> ServiceHook<T> createRemoteServiceHook(Class<T> serviceType, String hostname,
-            int port);
+            int port) throws IOException;
 }
