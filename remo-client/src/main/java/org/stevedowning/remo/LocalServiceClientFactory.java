8e4a433b2ef3b58bb1cef34b53398ab240db337d
Steve Downing <ssdowning@gmail.com>
2014-06-13 21:00:21 -0700

    LocalServiceClientFactory skeleton

---
+++ remo-client/src/main/java/org/stevedowning/remo/LocalServiceClientFactory.java
@@ -0,0 +1,15 @@
+package org.stevedowning.remo;
+
+import java.io.IOException;
+
+public class LocalServiceClientFactory implements ClientFactory {
+    public <T> T getRemoteService(Class<T> serviceType) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public <T> ServiceHook<T> createRemoteServiceHook(Class<T> serviceType) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
