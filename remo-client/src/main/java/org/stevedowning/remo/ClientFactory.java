fc4122fbb71196513098ede203be38dbf4342065
Steve Downing <ssdowning@gmail.com>
2014-06-16 11:40:46 -0700

    TODO

--- remo-client/src/main/java/org/stevedowning/remo/ClientFactory.java
+++ remo-client/src/main/java/org/stevedowning/remo/ClientFactory.java
@@ -3,6 +3,7 @@ package org.stevedowning.remo;
 import java.io.IOException;
 
 public interface ClientFactory {
+    // TODO: Change these names to not imply remoteness.
     public <T> T getRemoteService(Class<T> serviceType) throws IOException;
     public <T> ServiceHook<T> createRemoteServiceHook(Class<T> serviceType) throws IOException;
 }
