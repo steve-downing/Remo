f1475289cb20311248a0bb061592e2eb57fce8e5
Steve Downing <ssdowning@gmail.com>
2014-06-15 21:52:41 -0700

    TODO

--- remo-server/src/main/java/org/stevedowning/remo/ServiceHandle.java
+++ remo-server/src/main/java/org/stevedowning/remo/ServiceHandle.java
@@ -3,4 +3,5 @@ package org.stevedowning.remo;
 public interface ServiceHandle {
     public void safeShutdown();
     public boolean isRunning();
+    // TODO: public ServiceHandle onReady(Runnable runnable);
 }
