64750756a87990d944e35f82d268b79e69532a69
Steve Downing <ssdowning@gmail.com>
2014-06-16 12:27:59 -0700

    TODO

--- remo-common/src/main/java/org/stevedowning/remo/internal/common/invocation/RequestHandler.java
+++ remo-common/src/main/java/org/stevedowning/remo/internal/common/invocation/RequestHandler.java
@@ -4,5 +4,6 @@ import org.stevedowning.remo.Future;
 import org.stevedowning.remo.internal.common.request.Request;
 
 public interface RequestHandler {
+    // TODO: Maybe this would make more sense if it returned a Future<Response>.
     public Future<?> submitRequest(Request request);
 }
