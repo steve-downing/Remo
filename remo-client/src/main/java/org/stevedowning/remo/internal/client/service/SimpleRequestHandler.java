43743783369117de58e1c9ff3abdc06d49281669
Steve Downing <ssdowning@gmail.com>
2014-06-15 22:49:32 -0700

    TODO

--- remo-client/src/main/java/org/stevedowning/remo/internal/client/service/SimpleRequestHandler.java
+++ remo-client/src/main/java/org/stevedowning/remo/internal/client/service/SimpleRequestHandler.java
@@ -24,6 +24,8 @@ public class SimpleRequestHandler implements RequestHandler {
     public Future<?> submitRequest(Request request) {
         RequestBatch requestBatch = new RequestBatch(idFactory.generateId());
         requestBatch.add(request);
+        // TODO: It's possible that in the future, a ResponseBatch won't have a Response for every
+        //       Request in the RequestBatch.
         return conn.send(requestBatch).transform((ResponseBatch responseBatch) -> {
             String resultStr = responseBatch.get(request.getId()).getSerializedResult();
             return serializationManager.deserialize(resultStr);
