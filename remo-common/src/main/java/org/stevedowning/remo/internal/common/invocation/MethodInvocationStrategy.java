cc88f553a458cf3bbc0a1438089a8a7f45e7dcfe
Steve Downing <ssdowning@gmail.com>
2014-06-15 22:27:22 -0700

    Comment clarification

--- remo-common/src/main/java/org/stevedowning/remo/internal/common/invocation/MethodInvocationStrategy.java
+++ remo-common/src/main/java/org/stevedowning/remo/internal/common/invocation/MethodInvocationStrategy.java
@@ -16,6 +16,8 @@ public interface MethodInvocationStrategy {
                     throws IOException, InterruptedException, ExecutionException;
     public Object invokeServiceMethod(Method method, Object handler, Object[] args)
             throws Exception;
-    // Indicates whether getVal() can block on remote execution. This is important for batching correctly.
+    // Indicates whether getVal() can block on remote execution. This is important for batching
+    // correctly. When the client creates an explicit batch block, we need to be smart and fire off
+    // the batch if one of the calls blocks on a response from the service.
     // TODO: public boolean isBlockingCall();
 }
