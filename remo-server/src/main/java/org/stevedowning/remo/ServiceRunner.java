02d460d08417a0def62e160b7e023db37591287d
Steve Downing <ssdowning@gmail.com>
2014-08-27 11:53:04 -0700

    TODO

--- remo-server/src/main/java/org/stevedowning/remo/ServiceRunner.java
+++ remo-server/src/main/java/org/stevedowning/remo/ServiceRunner.java
@@ -3,6 +3,8 @@ package org.stevedowning.remo;
 import java.io.IOException;
 
 public interface ServiceRunner {
+    // TODO: Don't require the user to explicitly pass in a serviceContract.
+    //       If they don't, just assume that all of handler's interfaces should be used.
     public <T> ServiceHandle runService(T handler, Class<T> serviceContract, int port)
             throws IOException;
 }
