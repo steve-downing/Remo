688fab64f1579e73427194ad455c79117b9ecd7a
Steve Downing <ssdowning@gmail.com>
2014-09-17 18:50:54 -0700

    ClientId

---
+++ remo-common/src/main/java/org/stevedowning/remo/internal/common/ClientId.java
@@ -0,0 +1,49 @@
+package org.stevedowning.remo.internal.common;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.stevedowning.commons.idyll.Id;
+
+public class ClientId implements Id<Object> {
+    private long timestamp;
+    private long secret;
+    
+    private ClientId() {}
+    
+    public static ClientId generate() {
+        ClientId id = new ClientId();
+        id.timestamp = new Date().getTime();
+        id.secret = new Random().nextLong();
+        return id;
+    }
+    
+    public String toString() {
+        return timestamp + "-" + secret;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (secret ^ (secret >>> 32));
+        result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ClientId other = (ClientId) obj;
+        if (secret != other.secret)
+            return false;
+        if (timestamp != other.timestamp)
+            return false;
+        return true;
+    }
+}
