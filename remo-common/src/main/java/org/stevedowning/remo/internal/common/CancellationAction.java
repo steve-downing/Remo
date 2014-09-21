c50fade3701e93d4283cbdea23710a2907638f5b
Steve Downing <ssdowning@gmail.com>
2014-09-20 18:12:36 -0700

    Removing an unnecessary method

--- remo-common/src/main/java/org/stevedowning/remo/internal/common/CancellationAction.java
+++ remo-common/src/main/java/org/stevedowning/remo/internal/common/CancellationAction.java
@@ -1,6 +1,5 @@
 package org.stevedowning.remo.internal.common;
 
 public interface CancellationAction {
-    public default void run() { run(true); }
     public void run(boolean mayInterruptIfRunning);
 }
